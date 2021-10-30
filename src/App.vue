525ac37e850a68094361f1c934051c2c5a425744
rodriguesabner <abnerodrigs@gmail.com>
2021-10-30 00:22:30 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,156 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="top-content">
+      <h1>
+        Markdown WPP
+      </h1>
+      <label>
+        Tente escrever uma mensagem utilizando o markdown igual ao do whatsapp
+      </label>
+    </div>
+
+    <form @submit.prevent="sendMessage">
+      <input
+          style="margin-top: 1em"
+          placeholder="Escreva a sua mensagem"
+          v-model="message"
+      />
+
+      <button
+          type="submit"
+          style="margin-left: 10px; padding: 10px; cursor:pointer;"
+      >
+        Enviar Mensagem
+      </button>
+    </form>
+
+    <ul>
+      <li
+          ref="message"
+          class="textRef"
+          v-for="(messages, index) of messagesList"
+          :key="index"
+          :id="index"
+      >
+        {{ messages }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {FormatMarkdownService} from "wppconnect-markdown";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  mounted() {
+    this.formatMarkdownService(this.$refs.message);
+    //vai atualizar todas as msgs existentes
+  },
+  data: () => {
+    return {
+      messagesList: [
+        "mensagem 0",
+        "*mensagem 1*",
+        "*mensagem 2*",
+        "*mensagem 3*",
+        "*mensagem 4*"
+      ],
+      message: "",
+      formatMarkdownService: FormatMarkdownService
+    }
+  },
+  created() {
+    this.formatMarkdownService(this.$refs.message)
+  },
+  watch:{
+    messagesList: function(){
+      this.handleChangeMessageList();
+    }
+  },
+  methods: {
+    handleChangeMessageList(){
+      const lastIndex = this.messagesList.length - 1;
+      this.formatMarkdownService(null, lastIndex);
+    },
+    sendMessage() {
+      this.messagesList.push(this.message);
+      this.message = "";
+    }
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+  box-sizing: border-box;
+}
+html, body {
+  height: 100%;
+  background: #fff;
+  overflow: hidden;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: block;
+  overflow: auto;
+  height: 100%;
+  max-height: 100vh;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 540px;
+  margin: 0 auto;
+}
+.top-content {
+  padding: 2em;
+  color: #333;
+}
+h1 {
+  color: #42b983;
+  font-family: "Roboto Mono", serif;
+}
+button {
+  background: #42b983;
+  border-radius: 4px;
+  outline: 0;
+  border: 0;
+  padding: 0 2em !important;
+  height: 54px;
+  color: #000;
+  font-weight: 500;
+  text-transform: uppercase;
+}
+input {
+  padding: 0 10px;
+  outline: 0;
+  background: transparent;
+  border: 2px solid #F2F4F6;
+  border-radius: 4px;
+  height: 54px;
+  color: #333;
+}
+ul {
+  width: 100%;
+  list-style-type: none;
+  margin-top: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-bottom: 3em;
+}
+li {
+  width: 50%;
+  text-align: start;
+  color: #333;
+  padding: 10px;
+  background: #F2F4F6;
+  border-radius: 4px;
+  margin: auto;
+  margin-top: 1em;
 }
 </style>
